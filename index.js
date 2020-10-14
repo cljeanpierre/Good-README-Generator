83bc7005d1f50d402f5638e2c51c86f916c76d7a
Carla <carla@layc-dc.org>
2020-10-14 16:03:08 -0400

    added prompt questions to index.js

--- index.js
+++ index.js
@@ -4,7 +4,44 @@ const questions = [
         type: "input",
         name: "title",
         message: "What is the title of your project?"
-    }
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Provide a brief description of your project."
+    },
+    {
+        type: "list",
+        name: "table of contents",
+        default: ["Installation", "Usage", "License", "Contributing", "Tests", "Questions"]
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "What command will be used to install the dependencies?",
+        default: "npm install"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Provide the user any information needed to use the repository.",
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Which type of license will your project use?",
+        choices: ["MIT", "BSD-3-Clause", "ISC", "Apache-2.0", "Unlicensed"]
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Provide the user any information needed to make contributions to the repository."
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "What command will be used to run tests on your project?"
+    },
 
 ];
 
