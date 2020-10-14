11e1e415d9cfce77b4fa6b091d576d929fa5ca60
Carla <carla@layc-dc.org>
2020-10-14 17:39:04 -0400

    added function to generate Markdown for Good README

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -26,12 +26,47 @@ function assignLicenseSection (license) {
 }
 
 
-
-
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+${assignLicenseBadge (data.license)}
 
+Project Description-
+
+${data.description}
+
+Table of Contents:
+
+[Installation] (#installation)
+
+[Usage] (#usage)
+${assignLicenseLink (data.license)}
+
+[Contributing] (#contributing)
+
+[Tests](#tests)
+
+[Questions](#questions)
+
+Installation-
+
+To install the dependencies needed, type the following command:
+${data.installation}
+
+Usage-
+${data.usage}
+${assignLicenseSection(data.license)}
+
+Contributing- 
+${data.contributing}
+
+Tests-
+To perform a test of the application, type the following command:
+${data.test}
+
+Questions-
+If you have any questions about this repository, please contact me at ${data.email}.  
+For more information, please visit my GitHub page at ${data.github} (https://github.com/${data.github}/)
 `;
 }
 
