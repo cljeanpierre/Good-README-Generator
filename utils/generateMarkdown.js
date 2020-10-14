26f430fea999d00c177080abd30d905a5d1a64d7
Carla <carla@layc-dc.org>
2020-10-14 17:08:16 -0400

    added functions to assign license badge and link

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,3 +1,33 @@
+// function to generate license badge
+function assignLicenseBadge (license){
+  if (license !== "Unlicensed") {
+    return `![GitHub license] (https://img.shields.io/badge/license-${license}-blue.svg)`
+  }
+    return ''
+}
+
+function assignLicenseLink (license) {
+  if (license !== "Unlicensed") {
+    return (
+      `\n* [license] (#license)\n`
+    )
+  }
+  return ''
+}
+
+function assignLicenseSection (license) {
+  if (license !== "Unlicensed") {
+    return (
+      `## License
+      This project is licensed under ${license} license.`
+    )
+  }
+  return ''
+}
+
+
+
+
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
